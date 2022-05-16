321709954ac59b2971ae559bb46796b9d3a70380
yap8 <nebozhenko.arseny@gmail.com>
2022-05-17 02:14:10 +0300

    change section positioning

--- src/App.tsx
+++ src/App.tsx
@@ -9,52 +9,66 @@ const App = () => {
         <Title className="mb-6" variant="h1">
           Components
         </Title>
-        <Title className="mb-6">Buttons</Title>
-        <Title className="mb-6" variant="h4">
-          Variants
-        </Title>
-        <div className="mb-6">
-          <Button className="mr-4" onClick={() => alert('Regular button')}>
-            Regular
-          </Button>
-          <Button
-            className="mr-4"
-            onClick={() => alert('Outlined button')}
-            variant="outlined"
-          >
-            Outlined
-          </Button>
-          <Button onClick={() => alert('Contained button')} variant="contained">
-            Contained
-          </Button>
-        </div>
-        <Title className="mb-6" variant="h4">
-          Sizes
-        </Title>
-        <div className="flex items-center">
-          <Button
-            className="mr-4"
-            onClick={() => alert('Small button')}
-            size="small"
-            variant="outlined"
-          >
-            Small
-          </Button>
-          <Button
-            className="mr-4"
-            onClick={() => alert('Medium button')}
-            variant="outlined"
-          >
-            Medium
-          </Button>
-          <Button
-            onClick={() => alert('Large button')}
-            size="large"
-            variant="outlined"
-          >
-            Large
-          </Button>
-        </div>
+        <section className="text-center">
+          <Title className="mb-6">Buttons</Title>
+          <div className="flex items-center justify-center">
+            <section className="mr-8 border shadow p-4">
+              <Title className="mb-6" variant="h4">
+                Variants
+              </Title>
+              <div>
+                <Button
+                  className="mr-4"
+                  onClick={() => alert('Regular button')}
+                >
+                  Regular
+                </Button>
+                <Button
+                  className="mr-4"
+                  onClick={() => alert('Outlined button')}
+                  variant="outlined"
+                >
+                  Outlined
+                </Button>
+                <Button
+                  onClick={() => alert('Contained button')}
+                  variant="contained"
+                >
+                  Contained
+                </Button>
+              </div>
+            </section>
+            <section className="mr-8 border shadow p-4">
+              <Title className="mb-6" variant="h4">
+                Sizes
+              </Title>
+              <div>
+                <Button
+                  className="mr-4"
+                  onClick={() => alert('Small button')}
+                  size="small"
+                  variant="outlined"
+                >
+                  Small
+                </Button>
+                <Button
+                  className="mr-4"
+                  onClick={() => alert('Medium button')}
+                  variant="outlined"
+                >
+                  Medium
+                </Button>
+                <Button
+                  onClick={() => alert('Large button')}
+                  size="large"
+                  variant="outlined"
+                >
+                  Large
+                </Button>
+              </div>
+            </section>
+          </div>
+        </section>
       </Container>
     </section>
   );
