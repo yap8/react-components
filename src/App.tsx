943270737bd07e890d84157e87677cb85f207e11
yap8 <nebozhenko.arseny@gmail.com>
2022-05-17 01:32:44 +0300

    added button sizes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,47 @@ const App = () => {
       </section>
       <section className="container">
         <h2>Buttons</h2>
-        <Button>Click me!</Button>
+        <h3>Variants</h3>
+        <div>
+          <Button className="mr-4" onClick={() => alert('Regular button')}>
+            Regular
+          </Button>
+          <Button
+            className="mr-4"
+            onClick={() => alert('Outlined button')}
+            variant="outlined"
+          >
+            Outlined
+          </Button>
+          <Button onClick={() => alert('Contained button')} variant="contained">
+            Contained
+          </Button>
+        </div>
+        <h3>Sizes</h3>
+        <div className="flex items-center">
+          <Button
+            className="mr-4"
+            onClick={() => alert('Small button')}
+            size="small"
+            variant="outlined"
+          >
+            Small
+          </Button>
+          <Button
+            className="mr-4"
+            onClick={() => alert('Medium button')}
+            variant="outlined"
+          >
+            Medium
+          </Button>
+          <Button
+            onClick={() => alert('Large button')}
+            size="large"
+            variant="outlined"
+          >
+            Large
+          </Button>
+        </div>
       </section>
     </div>
   );
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,38 @@
+import { twMerge } from 'tailwind-merge';
+
 interface ButtonProps {
+  className?: string;
+  size?: 'small' | 'medium' | 'large';
+  variant?: 'outlined' | 'contained';
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({
+  className,
+  size,
+  variant,
+  children,
+  ...props
+}) => {
   return (
-    <button className="text-xl px-4 py-2 border rounded transition hover:bg-slate-50">
+    <button
+      className={twMerge(
+        'rounded transition',
+        variant === 'outlined'
+          ? 'border hover:bg-slate-50'
+          : variant === 'contained'
+          ? 'border hover:bg-slate-50'
+          : 'hover:bg-slate-50',
+        size === 'large'
+          ? 'text-3xl px-5 py-2'
+          : size === 'small'
+          ? 'text-base px-4 py-2'
+          : 'text-xl px-4 py-2',
+        className
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
